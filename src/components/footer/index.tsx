742dea090a00d75075b062bf525b41be33e40292
Michael Krøyserth-Simsø <mikke.korgen@gmail.com>
2020-05-27 00:17:58 +0200

    add facebook logo to footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+
 import { FacebookFeed } from './facebookFeed';
 import MapLocation from '../map/mapContainer';
+
+import facebookIcon from './facebook.svg';
 
 export const Footer: React.FunctionComponent = () => {
   return (
@@ -15,10 +18,15 @@ export const Footer: React.FunctionComponent = () => {
       </section>
       <footer className="main-footer wrapper">
         <div className="main-footer__container">
-          <section className="main-footer__primary">
-            <p>Jadda</p>
+          <section className="main-footer__primary"></section>
+          <section className="main-footer__secondary">
+            <a
+              href="https://www.facebook.com/Husetcafe"
+              className="social-icon"
+            >
+              <img src={facebookIcon} alt="" />
+            </a>
           </section>
-          <section className="main-footer__secondary"></section>
         </div>
       </footer>
     </>
