5310f88228509f58746cd1bdeb4985500c65d989
Michael Krøyserth-Simsø <mikke.korgen@gmail.com>
2020-06-06 20:57:22 +0200

    start on menu page, add some of menu items

--- src/pages/rooms/index.tsx
+++ src/pages/rooms/index.tsx
@@ -3,7 +3,7 @@ import { HeroComponent } from './hero';
 import { RoomBooking } from './booking';
 import { RoomsContainer } from './rooms';
 
-export const Rooms = () => {
+export const Rooms: React.FunctionComponent = () => {
   return (
     <div className="rooms">
       <HeroComponent />
