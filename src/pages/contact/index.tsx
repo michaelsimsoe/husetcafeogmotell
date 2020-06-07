7141bf9fbecf62f6bdb513fa1774980edf0c79c4
Michael Krøyserth-Simsø <mikke.korgen@gmail.com>
2020-06-07 02:13:40 +0200

    remove unused var

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 
