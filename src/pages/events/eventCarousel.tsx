a5b1efec9b3e4c6f699c19e9151c6932cde5e53f
Michael Krøyserth-Simsø <mikke.korgen@gmail.com>
2020-06-07 23:44:41 +0200

    pleasing the lineter

--- src/pages/events/eventCarousel.tsx
+++ src/pages/events/eventCarousel.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
-// Images
-import biff from './assets/biff.jpg';
-import sushi from './assets/sushi.jpg';
-import viltaften from './assets/viltaften.jpg';
-
 interface EventCarouselProps {
   images: string[];
 }
@@ -26,7 +21,7 @@ export const EventCarousel: React.FunctionComponent<EventCarouselProps> = ({
       {images.map((image) => {
         return (
           <div>
-            <img src={image} />
+            <img src={image} alt="from actual event" />
           </div>
         );
       })}
